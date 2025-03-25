4463412cefc508e3072f9552d3812ce2add54f4d
lstevensALG <lstevens@algomau.ca>
2025-03-25 17:05:30 -0400

    Update 1

---
+++ directory.php
@@ -0,0 +1,4 @@
+<!-- to make paths less complicated -->
+<?php
+    define('SITE_ROOT', __DIR__);
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include "./directory.php";
+    $webpageTitle = "Home";
+    include SITE_ROOT."/includes/header.php";
+?>
+
+<?php
+    include SITE_ROOT."/includes/footer.php";
+?>
