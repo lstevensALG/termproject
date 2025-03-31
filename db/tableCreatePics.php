26f00ac66f3a8c6858edca76cb0e41659e91a380
lstevensALG <lstevens@algomau.ca>
2025-03-31 13:46:39 -0400

    in progress table creation

--- db/tableCreatePics.php
+++ db/tableCreatePics.php
@@ -1,5 +1,7 @@
 <?php
     $sqlTableCreatePics = "CREATE TABLE IF NOT EXISTS termproject_pics
         (pic_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        )";
+        pic_path VARCHAR(50) NOT NULL,
+        pic_name VARCHAR(50) NOT NULL
+    )";
 ?>
--- db/tableCreatePicsSaved.php
+++ db/tableCreatePicsSaved.php
@@ -1,6 +1,7 @@
 <?php
     $sqlTableCreatePicsSaved = "CREATE TABLE IF NOT EXISTS termproject_pics_saved
         (save_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-        profile_id INTEGER FOREIGN KEY REFERENCES termproject_profiles(profile_id),
-        saved_pic )";
+        profile_id FOREIGN KEY REFERENCES termproject_profiles(profile_id),
+        pic_id FOREIGN KEY REFERENCES termproject_pics(pic_id)
+    )";
 ?>
