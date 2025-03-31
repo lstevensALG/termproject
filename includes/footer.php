9e0cb4cd5569bd0c8855f25cc52a0ab6be231170
lstevensALG <lstevens@algomau.ca>
2025-03-31 02:06:47 -0400

    Updating footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,24 @@
+        <div class="container relative-bottom text-justify mt-5">
+            <div class="row">
+                <div class="col-3">
+                    <h6 class="text-center">About Section</h6>
+                    <div class="text-muted">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat dolores debitis saepe id quibusdam itaque nostrum corporis obcaecati? Repudiandae incidunt aspernatur minus officia aliquam necessitatibus quaerat? Est ullam quos modi.</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    
+                </div>
+                <div class="col-3">
+                    <h6 class="text-center">Follow Us</h6>
+                    <div class="text-muted">
+                        <p>Facebook</p>
+                        <p>Instagram</p>
+                        <p>Snapchat</p>
+                        <p>TikTok</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
