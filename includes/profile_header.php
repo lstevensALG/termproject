ffff0ec2572a772e5448c5539475b4cabf2815ae
Somgolie <sejinkeonye@algomau.ca>
2025-03-31 23:41:38 -0400

    added profile header
    gave profile page its header

---
+++ includes/profile_header.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AnimalWeb</title>
+<link rel="stylesheet" href="/Formaction/css/bootstarp.css">
+<link rel="stylesheet" href="/Formaction/css/style.css">
+<style>
+.container {
+    
+    padding: 20px; /* Adds spacing inside the container */
+    border-radius: 10px; /* Optional: Rounds the corners */
+    width:2500px ;
+
+
+}
+.Space{
+    align-items: center;
+}
+</style>
+</head>
+<header class="mb-5">
+    <div class="container mt-8">
+        <div class="row">
+
+                <div class="col-md-8 discussion-menu d-flex">
+
+                    <ul class="nav">
+                        <li class="nav-itm">
+                            <a href="#" class="nav-link text-bg-primary rounded-3">
+                                Home</a>
+                        </li>
+                        <li class="nav-itm">
+                            <a href="#" class="nav-link ">
+                                Logout</a>
+                        </li>
+                        <li class="nav-itm">
+                            <a href="#" class="nav-link">
+                                About</a>
+                        </li>
+
+                    </ul>
+
+                    <div class="d-flex justify-content-end Space">
+                        <div class="btn-group">
+                            <button class="btn btn-secondary btn-sm" data-bs-theme-value="dark">Dark</button>
+                            <button class="btn btn-light btn-sm" data-bs-theme-value="light">Light</button>
+                        </div>
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+</header> 
