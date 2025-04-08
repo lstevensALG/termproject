64d2df5892d615814dff3ca29e3311f3322c4434
lstevensALG <lstevens@algomau.ca>
2025-04-08 15:01:59 -0400

    Proper pic table population

--- db/tablePopulatePics.php
+++ db/tablePopulatePics.php
@@ -6,26 +6,30 @@
     //include this file
 
     //include dbConnection.php before including this
+
     $sqlTableInsertPics = "INSERT INTO termproject_pics
     (pic_path, pic_name) 
     VALUES (?, '')";
 
     //check if table has records
-    $sqlTableSelectPics = "SELECT * FROM termproject_pics";
+    $sqlTableSelectPics = "SELECT pic_path FROM termproject_pics";
     $stmt = $conn->prepare($sqlTableSelectPics);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result->num_rows == 0) {
-        //if no records, populate table with all files in /pics
-        $dir = new DirectoryIterator(SITE_ROOT."/pics/");
-        foreach ($dir as $fileinfo) {
-            if (!$fileinfo->isDot()) {
-                $stmt = $conn->prepare($sqlTableInsertPics);
-                $value = $fileinfo->getFilename();
-                $stmt->bind_param("s", $value);
-                $stmt->execute();
-            }
+    $pic_array = [];
+    while ($record = $result->fetch_assoc()) {
+        $pic_array[] = $record['pic_path'];
+    }
+
+
+    $dir = new DirectoryIterator(SITE_ROOT."/pics/");
+    foreach ($dir as $fileinfo) {
+        $value = $fileinfo->getFilename();
+        if (!$fileinfo->isDot() && !in_array($value, $pic_array) ) {
+            $stmt = $conn->prepare($sqlTableInsertPics);
+            $stmt->bind_param("s", $value);
+            $stmt->execute();
         }
     }
     
